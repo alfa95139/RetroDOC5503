3f65e74089d9619fea59d0c5e1d2097a114c52dd
Alex <alessandro.fasan@gmail.com>
2021-02-17 23:43:23 -0800

    Update RetroDOC5503.ino
    Updated with emulation of CPU instructions

--- RetroDOC5503.ino
+++ RetroDOC5503.ino
@@ -12,10 +12,13 @@
 #define outputDEBUG     0
 
 
-byte PROGRAM[][3] = {
+byte PROGRAM[][5] = {
    {'W', 0x10, 0xAA},         // Write register 0x10 with value 0xAA
-   {'D', 0x08, 0x00},        // wait 08 clock cycles
-   {'R', 0x10, 0xAA},        // READ register 0x10, expect value 0xAA
+   {'D', 0x08},               // NOP for 8 cycles
+   {'R', 0x10, 0xA1},         // READ register 0x10, expect value 0xAA -> should flag that expected data is different (show received data)
+   {'d'},                     // NOP
+   {'r', 0x10},               // READ register 0x10, no expected value  
+   {'E'}          // END of program
 };
 
 /*
@@ -67,7 +70,18 @@ decrements at each iteration of loop().
 #define MEGA_PA1  (22)  // DOC A6
 #define MEGA_PA0  (15)  // DOC A7
 
-/* Digital Pin Assignments */
+/* Digital Pin Assignments 
+
+You can later optimize this by accessing the GPIOX_PDDR register directly.
+this is what we do for data bus.0=input, 1=output.
+
+#define xDATA_DIR_IN()    (GPIOD_PDDR = (GPIOD_PDDR & 0xFFFFFF00))       // same for RetroDOC
+#define xDATA_DIR_OUT()   (GPIOD_PDDR = (GPIOD_PDDR | 0x000000FF))       // same for RetroDOC
+
+So, modify above for xADDR_DIR_IN() and xADDR_DIR_OUT() using GPIOA_PDDR and the bitfields here.
+
+//#define ADDR_H_RAW      ((word) (GPIOA_PDIR & 0b1111000000100000))    // how do I change direction?
+#define ADDR_L_RAW        ((word) (GPIOC_PDIR & 0b0000111111011111))    // how to I change direction?
 
 // read bits raw
 #define xDATA_DIR_IN()    (GPIOD_PDDR = (GPIOD_PDDR & 0xFFFFFF00))       // same for RetroDOC
@@ -86,6 +100,14 @@ decrements at each iteration of loop().
 #define ADDR_in           ((word) (xDATA_DIR_IN() << 8 | ADDR_L_RAW))
 //#define ADDR_H            ((byte) ((ADDR & 0xFF00) >> 8))
 #define ADDR_L            ((byte) (ADDR & 0x00FF))
+
+*/
+
+#define SET_DATA_OUT(D)   (GPIOD_PDOR = (GPIOD_PDOR & 0xFFFFFF00) | (D)) // same for RetroDOC
+#define xDATA_IN          ((byte) (GPIOD_PDIR & 0xFF))    
+// same for RetroDOC
+//#define ADDR_L_RAW        ((word) (GPIOC_PDIR & 0b0000111111011111))    // how to I change direction?
+//#define ADDR_L            ((byte) (ADDR & 0x00FF))
 
 #define DOC_RESET_N  MEGA_PD7    // RST_ output to RetroDOC
 #define DOC_RW_N     MEGA_PG1    // RW_  output to RetroDOC
@@ -115,27 +137,59 @@ unsigned long clock_cycle_count;
 word DOC_ADDR;
 byte DOC_DATA;
 
+
+
+//  Set direction for CPU BUS, READ operation
+void xCPU_DIR_READ()
+{
+byte pinTable[16] = {
+    5,21,20,6,8,7,14,2,      // D7..D0
+    12,11,25,10,9,23,22,15}; // A7..A0
+
+for (int i=0; i<8; i++)
+    {
+      pinMode(pinTable[i],INPUT);
+      pinMode(pinTable[i+8],OUTPUT);
+    }
+}
+
+// Set direction for CPU BUS, WRITE operation
+void xCPU_DIR_WRITE()
+{
+byte pinTable[16] = {
+    5,21,20,6,8,7,14,2,      // D7..D0
+    12,11,25,10,9,23,22,15}; // A7..A0
+
+for (int i=0; i<8; i++)
+    {
+      pinMode(pinTable[i],OUTPUT);
+      pinMode(pinTable[i+8],OUTPUT);
+    }
+}
+
+// Set direction for DOC BUS
+void xDOC_BUS()
+{
+byte pinTable[16] = {
+    5,21,20,6,8,7,14,2,      // D7..D0
+    12,11,25,10,9,23,22,15}; // A7..A0
+
+for (int i=0; i<8; i++)
+    {
+      pinMode(pinTable[i],INPUT);
+      pinMode(pinTable[i+8],INPUT);
+    }
+}
+
+
 void DOC_init()
 {
-  // Set directions for ADDR & DATA Bus.
-  
-  byte pinTable[16] = {
-    5,21,20,6,8,7,14,2,      // D7..D0
-    12,11,25,10,9,23,22,15}; //  // A7..A0
+// Set directions for ADDR & DATA Bus.
+xCPU_DIR_WRITE();
 
-  
-  for (int i=0; i<16; i++)
-  {
-    pinMode(pinTable[i],INPUT);
-  } 
+// The following are fixed
 
-  /*   THIS WILL FLIP THE DIRECTION OF THE BUS
-  for (int i=0; i<16; i++)
-  {
-    pinMode(pinTable[i],OUTPUT);
-  } 
-  */
-
+// Input to CPU (Teensy) - Output from DOC
 pinMode(DOC_RAS_N,INPUT);
 pinMode(DOC_CAS_N,INPUT);
 pinMode(DOC_CSTRB_N,INPUT);
@@ -144,22 +198,23 @@ pinMode(DOC_CA1,INPUT);
 pinMode(DOC_CA2,INPUT);
 pinMode(DOC_CA3,INPUT);
 pinMode(DOC_GND,INPUT);
+pinMode(DOC_IRQ_N, INPUT);
+pinMode(DOC_Q, INPUT);
+pinMode(DOC_BS, INPUT);
+pinMode(DOC_E, INPUT);
+
+// Outputs of CPU (Teensy) - Input to DOC
+pinMode(DOC_CLK, OUTPUT);
 pinMode(DOC_RESET_N, OUTPUT);
 pinMode(DOC_RW_N, OUTPUT);
 pinMode(DOC_CS_N, OUTPUT);
-pinMode(DOC_BS, INPUT);
-pinMode(DOC_IRQ_N, INPUT);
-pinMode(DOC_Q, INPUT);
-pinMode(DOC_CLK, OUTPUT);
-pinMode(DOC_E, INPUT);
 
-  DOC_assert_reset();
+DOC_assert_reset();
 
-  clock_cycle_count = 0;
+clock_cycle_count = 0;
 }
 
-////
-
+///
 void DOC_assert_reset()
 {
   // Drive RESET conditions
@@ -168,7 +223,6 @@ void DOC_assert_reset()
 }
 
 ///
-
 void DOC_release_reset()
 {
   // Drive RESET conditions
@@ -187,6 +241,7 @@ volatile byte ADDRESS_IN;
 
 int clk;
 
+// doc_tick()
 inline __attribute__((always_inline))
 void doc_tick()
 {
@@ -231,50 +286,89 @@ void setup()
 ////////////////////////////////////////////////////////////////////
 // Loop
 ////////////////////////////////////////////////////////////////////
-int E, m_E = 0;
+int E, m_E = 0; 
+int PC = 0;
+bool cpu_halt = false;
 
 void loop()
 {
-  word j = 0;
+word j = 0;
 
-  // Loop forever
-  //  
-  DOC_ADDR = 0;
-  while(1)
-  {
-    doc_tick();
+// Loop forever
+//  
 
-    E = digitalRead(DOC_E);
-    if( (m_E ==0) & (E==1) ) {
-        Serial.printf("Posedege E detected: Time for the CPU to work here...\n");
-        //posedge E - here the CPU drives
-        // put WRITE REGS/READ REGS here
+while(1)
+        {
+        doc_tick();
 
-        // Update address
-        DOC_ADDR = DOC_ADDR++; // low 8 bits only
-        xDATA_DIR_IN();
-        // assert CS_
-        digitalWrite(DOC_CS_N, 0);
-        // *** READ *** 
-        digitalWrite(DOC_RW_N, 1); // READ
+        E = digitalRead(DOC_E);
+        if (!cpu_halt)
+                if( (m_E ==0) & (E==1) ) {
+                        Serial.printf("Posedege E detected: Time for the CPU to work here...\n");
+                        digitalWrite(DOC_CS_N, 0); // assert CS_
+                        switch(PROGRAM[0][PC]) {
+                                case 'd':       // ***  NOP  ***
+                                        Serial.printf("CPU (cycle %d): NOP\n", PC);
+                                break;
+                                case 'D': {      // *** NOP x CYCLES ***
+                                        Serial.printf("CPU (cycle %d): NOP\n", PC);
+                                }
+                                case 'W': {     // *** WRITE ***
+                                        Serial.printf("CPU (cycle %d): WRITE DOC register=%x, data=%x\n", PC, PROGRAM[1][PC], PROGRAM[2][PC] );
+                                        xCPU_DIR_WRITE();
+                                        SET_DATA_OUT(PROGRAM[2][PC]); 
+                                        digitalWrite(DOC_RW_N, 0); // WRITE to DOC Reg
+                                        delay(2);
+                                }
+                                break;
+                                case 'R': {     // *** READ VERIFY ***
+                                        Serial.printf("CPU (cycle %d):   READ DOC register=%x, EXPECTED data=%x, ", PC, PROGRAM[1][PC], PROGRAM[2][PC] );
+                                        xCPU_DIR_READ();
+                                        digitalWrite(DOC_RW_N, 1); // READ from DOC
+                                        // set address here   <<< HOW
+                                        delay(2);
+                                        while(DOC_RAS_N == 1);  // wait until DOC asserts RAS (active low)
+                                       
+                                        DATA_IN= xDATA_IN;
+                                        Serial.printf("READ %x\n", DATA_IN);
+                                        if (PROGRAM[2][PC] != DATA_IN) {        // Check expected data
+                                                Serial.printf("CPU ERROR ON CPU READ - CPU WILL HALT\n");
+                                                cpu_halt = true;        // Stop CPU on error
+                                                }
+                                }
+                                break;
+                                case 'e': {     // *** READ  ***
+                                        Serial.printf("CPU (cycle %d):   READ DOC register=%x, ", PC, PROGRAM[1][PC] );
+                                        xCPU_DIR_READ();
+                                        digitalWrite(DOC_RW_N, 1); // READ from DOC
+                                        // set address here    <<< HOW
+                                        delay(2);
+                                        while(DOC_RAS_N == 1);  // wait until DOC asserts RAS (active low)
+                                        DATA_IN= xDATA_IN;
+                                        Serial.printf("READ %x\n", DATA_IN);
+                                }
+                                case 'E':       // *** END PROGRAM ***
+                                        cpu_halt = true; // Stop CPU
+                                break;
+                                default: {      // *** INVALID COMMAND ****
+                                        Serial.printf("COMMAND NOT RECOGNIZED: CPU HALT\n");
+                                        cpu_halt = true; // Stop CPU
+                                }
+                        } // switch
 
-        while(DOC_RAS_N == 1);
-        DATA_IN= xDATA_IN;
-        Serial.printf("Reg %x, reading %x\n", DATA_IN);
-        // *** WRITE ***
-        // xDATA_DIR_OUT();
-        // digitalWrite(DOC_RW_N, 0); // WRITE to DOC Reg
-        // de-assert CS_
-        // digitalWrite(DOC_CS_N, 1);
-
-    }
+                PC++;                           // update Program Counter
+                delay(2);                       // short delay
+                digitalWrite(DOC_CS_N, 1);      // de-assert CS_
+                
+                } // posedge E
 
     if( (m_E==1) & (E==0) ) {
+        xDOC_BUS();
         Serial.printf("Negedge E detected: time for the DOC to work...\n");
-        //
         // negedege E - here the DOC drives
-        // check for ADDRESS and IRQ here
-    }
+        // Which means: A15 to A0 are input (output from DOC)
+        // check for ADDRESS, BS, CA3,CA2,CA1,CA0, CSTRB,  and IRQ here
+    } // negedge E
 
     m_E = E; // Do this here
 
